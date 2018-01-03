d5030337598cc522b545c8a331fb6264d5dfe1ae
Yong Li <yong.li1992@foxmail.com>
2018-01-03 22:59:27 +0000

    refactor search function

---
+++ src/js/util.js
@@ -0,0 +1,15 @@
+// A utility file for shared functionalities
+
+var rp = require("request-promise-native");
+
+/*
+ * A utility function to search a word.
+ * Currently we use shanbay API.
+ * See {https://api.shanbay.com/bdc/search/?word=hello}
+ * 
+ * @param {String} word The word to search.
+ * @return {Promise} p
+ */
+function searchWordP(word) {
+
+}
